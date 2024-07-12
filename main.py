2d1be9a71684f2601b14df4ba7239971901bee1a
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2024-07-12 14:29:59 +0530

    Remove CORS Policy

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# CORS middleware configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 @app.get("/")
 async def root():
     return {"Response":"ok"}
